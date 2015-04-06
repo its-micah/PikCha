82cf4e1c7334d488fe2ef3ee6c6e38174ca4c02c
camunity <cameron.flowers13@gmail.com>
2015-04-06 15:36:58 -0500

    Parse connected

--- PikCha/AppDelegate.m
+++ PikCha/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,8 +18,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Parse enableLocalDatastore];
+    [Parse setApplicationId:@"pyeXHaBOqNG4FofW3gkAwvyEUm9S90ZNTrZC6hIG"
+                  clientKey:@"PZZGqqWobS86vRGdyO5fAd3vhhYBdo7g7uU7tjDf"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
