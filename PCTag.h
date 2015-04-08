7314068d48bdb36c3e78dba1d20fd7ee1e44d71a
its-micah <mlanier86@gmail.com>
2015-04-08 11:53:55 -0500

    finished build

---
+++ PCTag.h
@@ -0,0 +1,18 @@
+//
+//  PCTag.h
+//  PikCha
+//
+//  Created by Micah Lanier on 4/8/15.
+//  Copyright (c) 2015 Micah Lanier Design and Illustration. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import "PCUser.h"
+#import "PCPhoto.h"
+
+@interface PCTag : PFObject
+
+@property PCUser *user;
+@property PCPhoto *photo;
+
+@end
---
+++ PCTag.m
@@ -0,0 +1,16 @@
+//
+//  PCTag.m
+//  PikCha
+//
+//  Created by Micah Lanier on 4/8/15.
+//  Copyright (c) 2015 Micah Lanier Design and Illustration. All rights reserved.
+//
+
+#import "PCTag.h"
+
+@implementation PCTag
+
+@dynamic user;
+@dynamic photo;
+
+@end
